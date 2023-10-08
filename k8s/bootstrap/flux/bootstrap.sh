41dfe0897967de4ecf5c2eef219018923ff49b35
Chris Bannister <c.bannister@gmail.com>
2023-10-08 17:02:43 +0100

    encrypt secrets with sops

---
+++ k8s/bootstrap/flux/bootstrap.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env /bin/bash
+
+set -euo pipefail
+
+kubectl apply --kustomize ./
+sops --decrypt sops-age.sops.yaml | kubectl apply -f -
+sops --decrypt github-deploy-key.sops.yaml | kubectl apply -f -
