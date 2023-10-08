17704e151b8c15a5542d9a219867b5147ab6ff70
Chris Bannister <c.bannister@gmail.com>
2023-10-08 19:40:43 +0100

    kick off the flux deploy of the cluster during bootstrap

--- k8s/bootstrap/flux/bootstrap.sh
+++ k8s/bootstrap/flux/bootstrap.sh
@@ -5,3 +5,6 @@ set -euo pipefail
 kubectl apply --kustomize ./
 sops --decrypt sops-age.sops.yaml | kubectl apply -f -
 sops --decrypt github-deploy-key.sops.yaml | kubectl apply -f -
+
+# off we go
+kubectl apply --kustomize ../../flux-config
