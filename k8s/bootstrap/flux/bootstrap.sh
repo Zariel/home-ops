3e358c64e6d95b196f174b0d6c06c29da9a9fed1
Chris Bannister <c.bannister@gmail.com>
2023-10-21 09:52:40 +0100

    switch to plex stable

---
+++ k8s/bootstrap/flux/bootstrap.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env /bin/bash
+
+set -euo pipefail
+
+kubectl apply --kustomize ./
+sops --decrypt sops-age.sops.yaml | kubectl apply -f -
+sops --decrypt github-deploy-key.sops.yaml | kubectl apply -f -
+
+# off we go
+kubectl apply --kustomize ../../flux-config
