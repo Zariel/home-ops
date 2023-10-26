1d4c1a56b8e52dc387b23606efef505881fcc1c9
Chris Bannister <c.bannister@gmail.com>
2023-10-26 16:59:29 +0100

    fix flux bootstrap

--- k8s/bootstrap/flux/bootstrap.sh
+++ k8s/bootstrap/flux/bootstrap.sh
@@ -7,4 +7,4 @@ sops --decrypt sops-age.sops.yaml | kubectl apply -f -
 sops --decrypt github-deploy-key.sops.yaml | kubectl apply -f -
 
 # off we go
-kubectl apply --kustomize ../../flux-config
+kubectl apply --kustomize ../../flux/config
