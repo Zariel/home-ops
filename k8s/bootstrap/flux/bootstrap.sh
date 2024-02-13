8508257f61b3ea321ec1826c414825e54969cd2c
Chris Bannister <c.bannister@gmail.com>
2024-02-13 22:46:28 +0000

    we talos now

--- k8s/bootstrap/flux/bootstrap.sh
+++ k8s/bootstrap/flux/bootstrap.sh
@@ -2,6 +2,11 @@
 
 set -euo pipefail
 
+kubectl apply --server-side --filename https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.71.2/example/prometheus-operator-crd/monitoring.coreos.com_podmonitors.yaml
+kubectl apply --server-side --filename https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.71.2/example/prometheus-operator-crd/monitoring.coreos.com_prometheusrules.yaml
+kubectl apply --server-side --filename https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.71.2/example/prometheus-operator-crd/monitoring.coreos.com_scrapeconfigs.yaml
+kubectl apply --server-side --filename https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.71.2/example/prometheus-operator-crd/monitoring.coreos.com_servicemonitors.yaml
+
 kubectl apply --kustomize ./
 sops --decrypt sops-age.sops.yaml | kubectl apply -f -
 sops --decrypt github-deploy-key.sops.yaml | kubectl apply -f -
